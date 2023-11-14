63893ddb08238351dffc4f3d7b9459a8852902b5
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-11-14 14:01:28 +0600

    Update yifysubtitles.ch movie subtitles download with zip .py

--- yifysubtitles.ch movie subtitles download with zip .py
+++ yifysubtitles.ch movie subtitles download with zip .py
@@ -1,7 +1,8 @@
+
 from bs4 import BeautifulSoup
+from lxml import html
 import os
 import requests
-from datetime import datetime
 
 # Base URL
 base_url = "https://yifysubtitles.ch/"
@@ -10,10 +11,8 @@ base_url = "https://yifysubtitles.ch/"
 if not os.path.exists("mysubfiles"):
     os.makedirs("mysubfiles")
 
-# Function to download subtitles from a list of URLs  
+# Function to download subtitles from a list of URLs
 def download_subtitles(url_list):
-    subtitles = []  # List to store downloaded subtitles
-
     for url in url_list:
         # Send an HTTP GET request to the URL
         response = requests.get(url)
@@ -39,14 +38,14 @@ def download_subtitles(url_list):
                 subtitle_response = requests.get(full_subtitle_url)
 
                 if subtitle_response.status_code == 200:
-                    # Parse the HTML content of the subtitle page using BeautifulSoup
-                    subtitle_soup = BeautifulSoup(subtitle_response.text, 'html.parser')
+                    # Parse the HTML content of the subtitle page using lxml
+                    subtitle_page_content = html.fromstring(subtitle_response.text)
 
-                    # Find the download link for the subtitles
-                    download_link = subtitle_soup.find('a', class_='srt')
+                    # Find the download link for the subtitles using the provided XPath
+                    download_link = subtitle_page_content.xpath('/html/body/div[3]/div[1]/div[3]/div[2]/div[4]/a/span[2]')
 
                     if download_link:
-                        download_url = download_link['href']
+                        download_url = download_link[0].getparent().get('href')
                         full_download_url = base_url + download_url
 
                         # Get the subtitle filename from the URL
@@ -61,7 +60,6 @@ def download_subtitles(url_list):
                                     for chunk in download_response.iter_content(chunk_size=1024):
                                         subtitle_file.write(chunk)
                                 print("Subtitle downloaded as:", subtitle_filepath)
-                                subtitles.append(subtitle_filepath)
                             else:
                                 print("Failed to download the subtitle.")
                     else:
@@ -73,32 +71,10 @@ def download_subtitles(url_list):
         else:
             print("Failed to retrieve the web page. Status code:", response.status_code)
 
-    return subtitles
-
 # List of URLs to download subtitles from
 url_list = [
-    "https://yifysubtitles.ch/movie-imdb/tt0111161",
-    "https://yifysubtitles.ch/movie-imdb/tt0068646",
-    "https://yifysubtitles.ch/movie-imdb/tt0468569",
-    "https://yifysubtitles.ch/movie-imdb/tt0071562",
-    "https://yifysubtitles.ch/movie-imdb/tt0050083",
-    "https://yifysubtitles.ch/movie-imdb/tt0108052"
+"https://yifysubtitles.ch/movie-imdb/tt0111161","https://yifysubtitles.ch/movie-imdb/tt0068646","https://yifysubtitles.ch/movie-imdb/tt0468569","https://yifysubtitles.ch/movie-imdb/tt0071562","https://yifysubtitles.ch/movie-imdb/tt0050083","https://yifysubtitles.ch/movie-imdb/tt0108052"
 ]
 
 # Call the function to download subtitles
-downloaded_subtitles = download_subtitles(url_list)
-
-# Merge the downloaded SRT files into one
-merged_subtitle = ""
-
-for subtitle_file in downloaded_subtitles:
-    with open(subtitle_file, 'r', encoding='utf-8') as file:
-        merged_subtitle += file.read()
-    merged_subtitle += "\n"  # Add a newline between subtitles
-
-# Save the merged subtitle as a single SRT file
-merged_subtitle_file = "merged_subtitles.srt"
-with open(merged_subtitle_file, 'w', encoding='utf-8') as file:
-    file.write(merged_subtitle)
-
-print("Merged subtitles saved as:", merged_subtitle_file)
+download_subtitles(url_list)
